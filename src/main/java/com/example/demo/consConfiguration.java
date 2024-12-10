af15851024ef5ead0a333f1c8b9f9eea475ccd2c
Kishore-nv <200801113@rajalakshmi.edu.in>
2024-12-10 14:19:24 +0530

    added origin for backend

--- src/main/java/com/example/demo/consConfiguration.java
+++ src/main/java/com/example/demo/consConfiguration.java
@@ -10,7 +10,7 @@ public class consConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-		.allowedOrigins("http://localhost:4200")
+		.allowedOrigins("http://localhost:4200","https://nostand-angular-git-bankai-kishorenvk22s-projects.vercel.app")
 		.allowedHeaders("*")
 		.allowedMethods("GET","POST","PUT","PATCH","DELETE");
 		

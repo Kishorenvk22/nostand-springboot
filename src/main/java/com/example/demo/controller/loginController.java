c410b6b91d8047362d455d29a21e56aabee0e6ab
Kishore-nv <200801113@rajalakshmi.edu.in>
2024-12-20 14:31:18 +0530

    updated login controller

--- src/main/java/com/example/demo/controller/loginController.java
+++ src/main/java/com/example/demo/controller/loginController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.loginDto;
+import com.example.demo.dto.loginUsername;
 import com.example.demo.entity.loginEntity;
 import com.example.demo.service.loginService;
 
@@ -31,6 +33,15 @@ public class loginController {
 	@GetMapping("/userinfo")
 	public List<loginDto> getAllUsers(){
 		return loginservice.findAllUsers();
+	}
+	
+	@PostMapping("/user")
+	public ResponseEntity<?> findUserName(@RequestBody loginDto logindto) {
+		loginEntity loginentity=loginservice.authenticateUser(logindto.getUsername(), logindto.getPassword());
+		if(loginentity!=null) {
+			return ResponseEntity.ok(new loginUsername(loginentity.getUsername()));
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
 	}
 	 
 	@PostMapping("/login")
